adf027284bba379be36b60d08d9edf5cc0d89d14
Sebastian de Jesus M <123346375+SDMsebastian@users.noreply.github.com>
2023-05-13 19:22:32 -0400

    Actualizar enviar.php
    Actualización de el php

--- contacto/enviar.php
+++ contacto/enviar.php
@@ -1,9 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nombre = $_POST["nombre"];
-  $email = $_POST["email"];
-  $mensaje = $_POST["mensaje"];
- 
-  echo "Gracias por contactarnos, $nombre. Te hemos enviado un correo electrónico a $email.";
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$para = 'propeto@gmail.com';
+$titulo = 'Nuevo comentario de ' . $nombre;
+$mensaje = 'Nombre: ' . $nombre . "\r\n" .
+           'Correo: ' . $correo . "\r\n" .
+           'Mensaje: ' . $mensaje;
+
+if (mail($para, $titulo, $mensaje)) {
+    echo 'El comentario se envió correctamente.';
+} else {
+    echo 'Error al enviar el comentario.';
 }
 ?>
